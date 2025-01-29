95de2219f477b0c3bcb11917a01d0d6998267724
Sanket-35 <sanketjnv35@gmail.com>
2025-01-29 08:26:14 +0530

    first commit

---
+++ src/test/java/demo/GitTest.java
@@ -0,0 +1,15 @@
+package demo;
+
+public class GitTest {
+
+	public static void main(String[] args) {
+
+		System.out.println("hello");
+
+	}
+	
+	public void test() {
+		System.out.println("git hub and git are seperate tools");
+	}
+
+}
